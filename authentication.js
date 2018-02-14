de3835ff4fcb804d8f44cfc06995e8ccff2fd16f
Blieper <yoranmandema@eml.cc>
2018-02-14 23:05:08 +0100

    fix

--- authentication.js
+++ authentication.js
@@ -11,7 +11,6 @@ exports.createUserDatabase = function (db, dbname) {
 
 exports.registerAndCheckUser = function (db, id, password) {
     let dbo = db.db(db.databaseName);
-    let foundUsers = [];
     let userExists = false;
     let user;
 
@@ -19,37 +18,30 @@ exports.registerAndCheckUser = function (db, id, password) {
     dbo.collection("users").find({}).toArray(function(err, result) {
         if (err) throw err;
 
-        console.log("1 Found users: " + result);
-
-        for (t in result) {
-            foundUsers.push(t);
+        if (result.length > 0) {
+            userExists = result.find(function(element){
+                user = element;
+                return element.steamid == id;
+            });
+    
+            console.log("Found user: " + user.steamid);
+    
+            if (!userExists) {  
+                dbo.collection("users").insertOne({steamid: id, pwd: password}, function(err, res) {
+                    if (err) throw err;
+                    console.log("User: " + id + " added!");
+                });
+            } else {
+                console.log("User: " + id + " already exists!");
+            }
+        }else{
+            dbo.collection("users").insertOne({steamid: id, pwd: password}, function(err, res) {
+                if (err) throw err;
+                console.log("No users found! User: " + id + " added!");
+            });
         }
     });
 
-    console.log("Found users: " + foundUsers);
-
-    if (foundUsers.length > 0) {
-        userExists = foundUsers.find(function(element){
-            user = element;
-            return element.steamid == id;
-        });
-
-        console.log("Found user: " + user.steamid);
-
-        if (!userExists) {  
-            dbo.collection("users").insertOne({steamid: id, pwd: password}, function(err, res) {
-                if (err) throw err;
-                console.log("User: " + id + " added!");
-            });
-        } else {
-            console.log("User: " + id + " already exists!");
-        }
-    }else{
-        dbo.collection("users").insertOne({steamid: id, pwd: password}, function(err, res) {
-            if (err) throw err;
-            console.log("No users found! User: " + id + " added!");
-        });
-    }
 
     return user;
 }
