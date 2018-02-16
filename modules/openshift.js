48adf90d2042bdf63c10cda46c74559b911d91db
Blieper <yoranmandema@eml.cc>
2018-02-16 17:29:37 +0100

    changed to port 3000

--- modules/openshift.js
+++ modules/openshift.js
@@ -3,7 +3,7 @@
 
 exports.init = function (app, process)
 {
-    var port            = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    var port            = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
         ip              = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
         mongoURL        = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
         mongoURLLabel   = "";
