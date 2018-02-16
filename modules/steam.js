d572f8645c4c22b6dad54e3bd6ee12959c44a0cd
Blieper <yoranmandema@eml.cc>
2018-02-16 14:05:24 +0100

    test

--- modules/steam.js
+++ modules/steam.js
@@ -4,8 +4,8 @@ exports.init = function (app){
     steam = require('steam-login');
 
     app.use(steam.middleware({
-        realm:  'localhost:8080', 
-        verify: 'localhost:8080/verify',
+        realm:  'http://nodejs-mongo-persistent-gmodcarregistration.193b.starter-ca-central-1.openshiftapps.com ', 
+        verify: 'http://nodejs-mongo-persistent-gmodcarregistration.193b.starter-ca-central-1.openshiftapps.com/verify',
         apiKey: '3E1190FDDEE75FCC8A5DA2650A07E06E'
         })
       );
