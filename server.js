aed340dd6fa043beabdf29c8247da64fc8eabbb5
Blieper <yoranmandema@eml.cc>
2018-02-19 17:16:05 +0100

    test

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.url = 'http://localhost:8080'
 
 // Importing seperate files for orginisation
 //var steamAuthentication   = require("./modules/steam").init(app);
-var openshift             = require("./modules/openshift").init(app, process);
-var mongo                 = require("./modules/mongo").init(app);
+//var openshift             = require("./modules/openshift").init(app, process);
+//var mongo                 = require("./modules/mongo").init(app);
 //var pagecounter           = require("./modules/pagecount").init(app);
 //var api                   = require("./modules/api").init(app);
 
