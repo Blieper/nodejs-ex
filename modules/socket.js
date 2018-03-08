4794ff1900843bf5e0858c2b70f4843a9e3c297c
Blieper <yoranmandema@eml.cc>
2018-03-08 22:08:36 +0100

    added serverside country checking

--- modules/socket.js
+++ modules/socket.js
@@ -5,6 +5,16 @@ exports.init = function (app, process) {
         server = require('http').createServer(app),
         request = require('request');
 
+    let countries = "United States of America, Afghanistan, Albania, Algeria, Andorra, Angola, Antigua & Deps, Argentina, Armenia, Australia, Austria, Azerbaijan, Bahamas, Bahrain, Bangladesh, Barbados, Belarus, Belgium, Belize, Benin, Bhutan, Bolivia, Bosnia Herzegovina, Botswana, Brazil, Brunei, Bulgaria, Burkina, Burma, Burundi, Cambodia, Cameroon, Canada, Cape Verde, Central African Rep, Chad, Chile, People's Republic of China, Republic of China, Colombia, Comoros, Democratic Republic of the Congo, Republic of the Congo, Costa Rica,, Croatia, Cuba, Cyprus, Czech Republic, Danzig, Denmark, Djibouti, Dominica, Dominican Republic, East Timor, Ecuador, Egypt, El Salvador, Equatorial Guinea, Eritrea, Estonia, Ethiopia, Fiji, Finland, France, Gabon, Gaza Strip, The Gambia, Georgia, Germany, Ghana, Greece, Grenada, Guatemala, Guinea, Guinea-Bissau, Guyana, Haiti, Holy Roman Empire, Honduras, Hungary, Iceland, India, Indonesia, Iran, Iraq, Republic of Ireland, Israel, Italy, Ivory Coast, Jamaica, Japan, Jonathanland, Jordan, Kazakhstan, Kenya, Kiribati, North Korea, South Korea, Kosovo, Kuwait, Kyrgyzstan, Laos, Latvia, Lebanon, Lesotho, Liberia, Libya, Liechtenstein, Lithuania, Luxembourg, Macedonia, Madagascar, Malawi, Malaysia, Maldives, Mali, Malta, Marshall Islands, Mauritania, Mauritius, Mexico, Micronesia, Moldova, Monaco, Mongolia, Montenegro, Morocco, Mount Athos, Mozambique, Namibia, Nauru, Nepal, Newfoundland, Netherlands, New Zealand, Nicaragua, Niger, Nigeria, Norway, Oman, Ottoman Empire, Pakistan, Palau, Panama, Papua New Guinea, Paraguay, Peru, Philippines, Poland, Portugal, Prussia, Qatar, Romania, Rome, Russian Federation, Rwanda, St Kitts & Nevis, St Lucia, Saint Vincent & the, Grenadines, Samoa, San Marino, Sao Tome & Principe, Saudi Arabia, Senegal, Serbia, Seychelles, Sierra Leone, Singapore, Slovakia, Slovenia, Solomon Islands, Somalia, South Africa, Spain, Sri Lanka, Sudan, Suriname, Swaziland, Sweden, Switzerland, Syria, Tajikistan, Tanzania, Thailand, Togo, Tonga, Trinidad & Tobago, Tunisia, Turkey, Turkmenistan, Tuvalu, Uganda, Ukraine, United Arab Emirates, United Kingdom, Uruguay, Uzbekistan, Vanuatu, Vatican City, Venezuela, Vietnam, Yemen, Zambia, Zimbabwe";
+
+    // Formatting country list
+    app.countryList = [];
+    countries = countries.split(',');
+
+    for (i of countries) {
+        app.countryList.push({ text: i.trim(), value: i.trim() });
+    }
+
     server.listen(app.port, app.ip, function () { // or define ip and port manually   
         var io = require('socket.io')(server);
 
@@ -43,15 +53,15 @@ exports.init = function (app, process) {
                 });
             })
 
-            socket.on("request_regions", data => {
+            socket.on("request_registerdata", data => {
                 regions = [
-                    {text: 'Cre8ive', value: 'Cre8ive'},
-                    {text: 'Daemon Effect', value: 'Daemon Effect'},
-                    {text: 'Vehicle Heaven', value: 'Vehicle Heaven'},
-                    {text: 'Genesis Build', value: 'Genesis Build'},        
+                    { text: 'Cre8ive', value: 'Cre8ive' },
+                    { text: 'Daemon Effect', value: 'Daemon Effect' },
+                    { text: 'Vehicle Heaven', value: 'Vehicle Heaven' },
+                    { text: 'Genesis Build', value: 'Genesis Build' },
                 ];
 
-                socket.emit('get_regions', regions);
+                socket.emit('get_registerdata', {regions: regions, countries: app.countryList});
             });
 
             socket.on("register_vehicle", data => {
@@ -94,7 +104,7 @@ exports.init = function (app, process) {
                             errorObject.region = "invalid";
                         }
 
-                        if (data.country == "nothing" || !data.country) {
+                        if (data.country == "nothing" || !data.country || countryList.indexOf(data.country) > -1) {
                             hasError = true;
                             errorObject.country = "invalid";
                         }
@@ -102,7 +112,7 @@ exports.init = function (app, process) {
                         if (data.name.length == 0) {
                             hasError = true;
                             errorObject.name = "invalid";
-                        }                        
+                        }
 
                         if (hasError) {
                             socket.emit("register_error", errorObject);
