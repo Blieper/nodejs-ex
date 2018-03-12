48ef206808b4d243ba36d56d453a9b95b1e55462
Blieper <yoranmandema@eml.cc>
2018-03-12 16:49:29 +0100

    main.js cleanup

--- views/javascript/main.js
+++ views/javascript/main.js
@@ -6,15 +6,18 @@ function Main() {
     console.log("RDY");
 
     function LoadInnerHTML(pageFile,objectID) { // objectID: the object in which the html file will be loaded; optional* default: #page-content
-
-        if (!pageFile) { return null; }
+        if (pageFile == null) {return}
+        
         objectID = objectID || "#page-content";
         // Loads the required page
         $(objectID).load("/pages/" + pageFile + ".html");
-
     }
 
     LoadInnerHTML(pageFile);
+
+    // Removes any script used to predefine variables, like 'pageFile' and 'user'
+    // This means the client won't see those ugly scripts :ok_hand:
+    $(".predef").remove();
 
 }
 
